325ee242a7d3ded6c0ca78644e7659f6a2a78308
YURISAMY <160258810+YURISAMY@users.noreply.github.com>
2024-08-08 16:15:54 -0300

    Update and rename convert_to_html.py to convert_to_JSON.py

---
+++ Scripts_python/convert_to_JSON.py
@@ -0,0 +1,21 @@
+import zipfile as zipfile 
+import pandas as pd
+
+for_concat = []
+
+with zipfile.ZipFile ('Documents/projetos/Bolsa/jupyternotebooks/postos.zip', 'r') as t:
+  
+    for arq in t.namelist():
+
+        if arq.endswith('.txt'):
+
+            with t.open(arq) as f:
+
+                df = pd.read_csv(f,delimiter = ';')
+
+                for_concat.append(df)
+
+registros_chuvas = pd.concat(for_concat)
+registros_chuvas.reset_index(drop=True, inplace=True)
+registros_chuvas.to_json('registros_chuvas.json')
+
--- Scripts_python/convert_to_html.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-Municio = pd.read_csv('C:\Users\yurii\Documents\GitHub\respositorio erosão costeira\CSV archives\postos\1.txt', delimiter = ';')
-
-Municio.to_html('C:\Users\yurii\Documents\GitHub\respositorio erosão costeira\CSV archives\postos\1.txt.html')
-
